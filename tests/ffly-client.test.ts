3ea27b14d97f8ea9028ab11dd213be67d74e01f7
YameenMalik <yameen@seed.im>
2022-05-18 16:09:49 +0500

    added tests

--- tests/ffly-client.test.ts
+++ tests/ffly-client.test.ts
@@ -1,51 +1,83 @@
 import { expect } from "chai";
 import { Networks, MARKET_SYMBOLS, FFLYClient } from "../src/index";
 
+const testAcctKey = "4ef06568055d528efdeb3a2e0c1a4b1a0f1fdf4f9e388f11f0a248228298c2b7";
+const testOrdersContract = "0x46ABa007B8c0ff7Da3132b52b81d2C15D2e8E815";
+
 describe("FFLYClient", () => {
 
     it("should initialize the client", async ()=>{
-        const client = new FFLYClient(Networks.TESTNET);
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
         expect(client).to.be.not.eq(undefined);
     });
 
-
     it("should add DOT-PERP market", async ()=>{
-        const client = new FFLYClient(Networks.TESTNET);
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
         expect(client.addMarket(MARKET_SYMBOLS.DOT)).to.be.equal(true);
     });
 
     it("should add DOT-PERP market with custom orders contract address", async ()=>{
-        const client = new FFLYClient(Networks.TESTNET);
-        expect(client.addMarket(MARKET_SYMBOLS.DOT, 
-            "0x46ABa007B8c0ff7Da3132b52b81d2C15D2e8E815")).to.be.equal(true);
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
+        expect(client.addMarket(MARKET_SYMBOLS.DOT, testOrdersContract)).to.be.equal(true);
     });
 
     it("should return FALSE as there is no market for name TEST-PERP", async ()=>{
-        const client = new FFLYClient(Networks.TESTNET);
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
         expect(client.addMarket("TEST-PERP")).to.be.equal(false);
     });
 
     it("should add market despite not existing in deployed contracts", async ()=>{
-        const client = new FFLYClient(Networks.TESTNET);
-        expect(client.addMarket("TEST-PERP",
-            "0x46ABa007B8c0ff7Da3132b52b81d2C15D2e8E815")).to.be.equal(true);
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
+        expect(client.addMarket("TEST-PERP", testOrdersContract)).to.be.equal(true);
     });
 
     it("should return False as DOT-PERP market is already added", async ()=>{
-        const client = new FFLYClient(Networks.TESTNET);
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
         expect(client.addMarket(MARKET_SYMBOLS.DOT)).to.be.equal(true);
         expect(client.addMarket(MARKET_SYMBOLS.DOT)).to.be.equal(false);
     });
 
     it("should remove the DOT market", async ()=>{
-        const client = new FFLYClient(Networks.TESTNET);
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
         expect(client.addMarket(MARKET_SYMBOLS.DOT)).to.be.equal(true);
         expect(client.removeMarket(MARKET_SYMBOLS.DOT)).to.be.equal(true);
     });
 
     it("should return false when trying to remove a non-existent market", async ()=>{
-        const client = new FFLYClient(Networks.TESTNET);
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
         expect(client.removeMarket(MARKET_SYMBOLS.DOT)).to.be.equal(false);
     });
 
+    it("should return USDC balance", async ()=>{
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
+        expect(await client.getUSDCBalance()).to.be.greaterThanOrEqual(0);
+    });
+
+    it("should get Test USDCs", async ()=>{
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
+        expect(await client.getTestUSDC()).to.be.equal(true);
+    });
+
+    it("should return USDCs in Margin Bank", async ()=>{
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
+        expect(await client.getMarginBankBalance()).to.be.greaterThanOrEqual(0);
+    });
+
+    it("should move 1 USDC token to Margin Bank", async ()=>{
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
+        expect(await client.moveUSDCToMarginBank(1)).to.be.equal(true);
+        expect(await client.getMarginBankBalance()).to.be.greaterThanOrEqual(1);
+    });
+
+    it("should move 1 USDC token from Margin Bank", async ()=>{
+        const client = new FFLYClient(Networks.TESTNET, testAcctKey);
+        expect(await client.moveUSDCFromMarginBank(1)).to.be.equal(true);
+    });
+
+
+    
+
+
+    
+    
 });
