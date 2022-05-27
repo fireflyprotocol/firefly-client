569f467b913a2040c9441c2d2d63c5db31f1db39
YameenMalik <yameen@seed.im>
2022-05-27 10:13:32 +0500

    exported on chain interface

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,7 @@ export * from "./signer/orderSigner";
 export * from "./helpers/utils";
 export * from "./types";
 export * from "./constants";
-// eslint-disable-next-line import/no-cycle
-export * from "./fireflyClient";
 export * from "../contracts/orderbook";
 export * from "./interfaces/routes";
 export * from "./interfaces/order";
+export * from "./interfaces/on-chain";
