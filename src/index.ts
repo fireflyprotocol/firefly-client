61ba50058bec17d4405591dfab905fe8f95961ba
YameenMalik <yameen@seed.im>
2022-05-29 03:36:11 +0500

    added comments

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,30 @@
+// signer
 export * from "./signer/orderSigner";
-export * from "./helpers/utils";
-export * from "./types";
-export * from "./constants";
-export * from "../contracts/orderbook";
+export * from "./signer/balance";
+export * from "./signer/baseValue";
+
+// interfaces
 export * from "./interfaces/routes";
 export * from "./interfaces/order";
 export * from "./interfaces/on-chain";
+
+// helpers
+export * from "./helpers/utils";
+export * from "./helpers/bytes";
+export * from "./helpers/signature";
+
+// api
+export * from "./api/service";
+export * from "./api/urls";
+
+// firefly client
 export * from "./fireflyClient";
+
+// types
+export * from "./types";
+
+// constants
+export * from "./constants";
+
+// contracts
+export * from "../contracts/orderbook";
