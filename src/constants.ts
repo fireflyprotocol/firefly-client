d41c93a033ee5aa6e9639e2bf180ffc161df4eba
Kulsoom Malik <kulsoom@seed.im>
2022-09-14 13:31:29 +0300

    socket url fixed for dapi, bumped version

--- src/constants.ts
+++ src/constants.ts
@@ -4,27 +4,27 @@ export const Networks = {
     chainId: 1297,
     apiGateway: "https://dapi-testnet.firefly.exchange",
     socketURL: "wss://dapi-testnet.firefly.exchange",
-    onboardingUrl: "https://testnet.firefly.exchange"
+    onboardingUrl: "https://testnet.firefly.exchange",
   },
   DEV: {
     url: "https://l2-dev.firefly.exchange/",
     chainId: 78602,
     apiGateway: "https://dapi-dev.firefly.exchange",
-    socketURL: "wss://dev.firefly.exchange/",
-    onboardingUrl: "https://dev.firefly.exchange"
+    socketURL: "wss://dapi-dev.firefly.exchange/",
+    onboardingUrl: "https://dev.firefly.exchange",
   },
   SANDBOX: {
     url: "https://l2-dev.firefly.exchange/",
     chainId: 78602,
     apiGateway: "https://dapi-dev-sandbox.firefly.exchange",
     socketURL: "wss://dapi-dev-sandbox.firefly.exchange/",
-    onboardingUrl: "https://dev-sandbox.firefly.exchange"
+    onboardingUrl: "https://dev-sandbox.firefly.exchange",
   },
   PRODUCTION: {
     url: "",
     chainId: 0,
     apiGateway: "",
     socketURL: "",
-    onboardingUrl: ""
-  }
+    onboardingUrl: "",
+  },
 };
