d64fccc292dbfd170eb145f049192a84928488bf
Talha Ahmed <talhaah5@gmail.com>
2022-12-26 14:23:21 +0500

    [EXC-221] Changed Websocket Address To public Domain And Port  To `2087` (#99)

--- src/constants.ts
+++ src/constants.ts
@@ -6,7 +6,7 @@ export const Networks = {
     chainId: 78602,
     apiGateway: "https://dapi-dev.firefly.exchange",
     socketURL: "wss://dapi-dev.firefly.exchange",
-    webSocketURL: "wss://k8s-albdapi-775184c5a7-538590878.us-east-1.elb.amazonaws.com:2063",
+    webSocketURL: "wss://dapi-dev.firefly.exchange:2087",
     onboardingUrl: "https://dev.firefly.exchange",
   },
   TESTNET_ARBITRUM: {
@@ -14,7 +14,7 @@ export const Networks = {
     chainId: 421613,
     apiGateway: "https://dapi-testnet.firefly.exchange",
     socketURL: "wss://dapi-testnet.firefly.exchange",
-    webSocketURL: "",
+    webSocketURL: "wss://dapi-testnet.firefly.exchange:2087",
     onboardingUrl: "https://testnet.firefly.exchange",
   },
   PRODUCTION_BOBA: {
@@ -22,7 +22,7 @@ export const Networks = {
     chainId: 1294,
     apiGateway: "https://dapi.firefly.exchange",
     socketURL: "wss://dapi.firefly.exchange",
-    webSocketURL: "",
+    webSocketURL: "wss://dapi.firefly.exchange:2087",
     onboardingUrl: "https://trade.firefly.exchange",
   },
   PRODUCTION_ARBITRUM: {
@@ -30,7 +30,7 @@ export const Networks = {
     chainId: 42161,
     apiGateway: "https://dapi-arb.firefly.exchange",
     socketURL: "wss://dapi-arb.firefly.exchange",
-    webSocketURL: "",
+    webSocketURL: "wss://dapi-arb.firefly.exchange:2087",
     onboardingUrl: "https://trade-arb.firefly.exchange",
   },
 };
