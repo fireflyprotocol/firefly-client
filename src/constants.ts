50d342a26a96f8d3b8762e038955a5370ebc6aa7
talha <talha@seed.im>
2023-01-23 15:14:36 +0500

    changed socket io to old domain

--- src/constants.ts
+++ src/constants.ts
@@ -13,7 +13,7 @@ export const Networks = {
     url: "https://arbitrum-goerli.infura.io/v3/62bcda18381b45eab5435e1342da21a6",
     chainId: 421613,
     apiGateway: "https://api.arbitrum-staging.firefly.exchange/dapi-server",
-    socketURL: "wss://api.arbitrum-staging.firefly.exchange/dapi-server",
+    socketURL: "wss://dapi-testnet.firefly.exchange",
     webSocketURL: "wss://api.arbitrum-staging.firefly.exchange/dapi-server-notifier/",
     onboardingUrl: "https://testnet.firefly.exchange",
   },
