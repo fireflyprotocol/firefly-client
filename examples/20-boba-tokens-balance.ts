e2179466a65331c8843e19985267572669a76fda
Kulsoom Malik <kulsoom@seed.im>
2023-03-09 21:28:11 +0500

    Removed biconomy implementation

--- examples/20-boba-tokens-balance.ts
+++ examples/20-boba-tokens-balance.ts
@@ -1,17 +1,17 @@
 /**
- * Checks BOBA(gas) tokens balance
+ * Checks Native(gas) tokens balance
  */
 
 /* eslint-disable no-console */
 import { Networks, FireflyClient, bnStrToBaseNumber } from "../index";
 
 async function main() {
-    // ensure that account has enough BOBA/MOVR tokens to perform on-chain USDC.mint() call
+    // ensure that account has enough native gas tokens to perform on-chain USDC.mint() call
     const dummyAccountKey =
     "a182091b4d5a090b65d604e36f68629a692e3bf2aa864bd3f037854034cdd676";
 
     // using TESTNET network, fundGas() does not work on MAINNET
-    const client = new FireflyClient(true, Networks.TESTNET_BOBA, dummyAccountKey); //passing isTermAccepted = true for compliance and authorizarion
+    const client = new FireflyClient(true, Networks.TESTNET_ARBITRUM, dummyAccountKey); //passing isTermAccepted = true for compliance and authorizarion
     await client.init()
     
     console.log(
