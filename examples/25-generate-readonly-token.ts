461a90b2e079e6b8bf0d575d0f3dcc9f13a2c2a7
Nida Karim Ali <nidakarimali@ip-192-168-1-104.ec2.internal>
2023-06-19 01:08:38 +0500

    added example code for generate readonly token

---
+++ examples/25-generate-readonly-token.ts
@@ -0,0 +1,22 @@
+/**
+ * Get symbols available on testnet
+ */
+
+/* eslint-disable no-console */
+import { Networks, FireflyClient } from "../index";
+
+async function main() {
+  // ensure that account has enough native gas tokens to perform on-chain USDC.mint() call
+  const dummyAccountKey =
+    "4d6c9531e0042cc8f7cf13d8c3cf77bfe239a8fed95e198d498ee1ec0b1a7e83";
+
+  // using TESTNET network
+  const client = new FireflyClient(true, Networks.TESTNET_ARBITRUM, dummyAccountKey); //passing isTermAccepted = true for compliance and authorizarion
+  await client.init()
+
+  // generate a read-only token for user which can be used to provide read-only access to data to non-owners
+  const response = await client.generateReadOnlyToken();
+  console.log("Readonly-token for user:", response.data);
+}
+
+main().then().catch(console.warn);
