153818287b0d13fc85df0dbde191a298b17073e8
Nida Karim Ali <nidakarimali@Nidas-MacBook-Pro.local>
2023-08-10 13:40:42 +0500

    refactored

--- examples/22-listening-events.ts
+++ examples/22-listening-events.ts
@@ -19,7 +19,7 @@ async function main() {
   const dummyAccountKey =
     "a182091b4d5a090b65d604e36f68629a692e3bf2aa864bd3f037854034cdd676";
 
-  const client = new FireflyClient(true, Networks.PRODUCTION_ARBITRUM, dummyAccountKey); // passing isTermAccepted = true for compliance and authorizarion
+  const client = new FireflyClient(true, Networks.TESTNET_ARBITRUM, dummyAccountKey); // passing isTermAccepted = true for compliance and authorizarion
   await client.init();
 
   client.addMarket(MARKET_SYMBOLS.ETH);
