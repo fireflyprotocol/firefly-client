7500f48d0128e4f7ebf2bf3149229e82f9ecd34b
Nida Karim Ali <nidakarimali@Nidas-MacBook-Pro.local>
2023-08-10 17:28:48 +0500

    updated

--- examples/22-listening-events.ts
+++ examples/22-listening-events.ts
@@ -10,37 +10,42 @@ import {
   GetOrderBookResponse,
   PlaceOrderResponse,
   ORDER_SIDE,
-  ORDER_TYPE
+  ORDER_TYPE,
 } from "../index";
-
 
 async function main() {
   // no gas fee is required to create order signature.
   const dummyAccountKey =
     "a182091b4d5a090b65d604e36f68629a692e3bf2aa864bd3f037854034cdd676";
 
-  const client = new FireflyClient(true, Networks.TESTNET_ARBITRUM, dummyAccountKey); // passing isTermAccepted = true for compliance and authorizarion
+  const client = new FireflyClient(
+    true,
+    Networks.TESTNET_ARBITRUM,
+    dummyAccountKey
+  ); // passing isTermAccepted = true for compliance and authorizarion
   await client.init();
 
   client.addMarket(MARKET_SYMBOLS.ETH);
-
 
   const connection_callback = async () => {
     console.log("Sockets connected");
     // start listening to global market and local user events
     client.sockets.subscribeGlobalUpdatesBySymbol(MARKET_SYMBOLS.ETH);
     client.sockets.subscribeUserUpdateByToken();
-  }
+  };
 
   const disconnection_callback = async () => {
     console.log("Sockets disconnected");
-    await client.cancelAllOpenOrders(MARKET_SYMBOLS.ETH);
-
-  }
+    try {
+      await client.cancelAllOpenOrders(MARKET_SYMBOLS.ETH);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   // adding listeners
-  await client.sockets.listen("connect",connection_callback);
-  await client.sockets.listen("disconnect",disconnection_callback);
+  await client.sockets.listen("connect", connection_callback);
+  await client.sockets.listen("disconnect", disconnection_callback);
 
   // create socket connection
   client.sockets.open();
@@ -48,16 +53,18 @@ async function main() {
     console.log("OrderUpdate:", order);
     // kill sockets in order to stop script
     client.sockets.close();
-
   };
-  const callbackOrderBookUpdates = ({ orderbook }: { orderbook: GetOrderBookResponse }) => {
+  const callbackOrderBookUpdates = ({
+    orderbook,
+  }: {
+    orderbook: GetOrderBookResponse;
+  }) => {
     console.log("OrderbookState:", orderbook);
   };
 
   const callbackExchangeHealth = ({ isAlive }: { isAlive: boolean }) => {
     console.log("Exchange Health:", isAlive);
   };
-
 
   client.sockets.onUserOrderUpdate(callbackOrderUpdates);
   client.sockets.onOrderBookUpdate(callbackOrderBookUpdates);
