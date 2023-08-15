02ef768ebb596d889164751239cdc890590199ff
Nida Karim Ali <nidakarimali@Nidas-MacBook-Pro.local>
2023-08-15 13:34:42 +0500

    added folder for socket examples

--- examples/22-postOrderKMS.ts
+++ examples/22-postOrderKMS.ts
@@ -4,32 +4,38 @@
  */
 
 /* eslint-disable no-console */
-import { Networks, FireflyClient, MARKET_SYMBOLS, ORDER_SIDE, ORDER_TYPE } from "../index";
 import { AwsKmsSigner } from "ethers-aws-kms-signer";
+import {
+  Networks,
+  FireflyClient,
+  MARKET_SYMBOLS,
+  ORDER_SIDE,
+  ORDER_TYPE,
+} from "../index";
 
 async function main() {
+  // for keyId input the arn of the kms key that have to be used.
+  const kmsSigner = new AwsKmsSigner({
+    region: "ap-northeast-1",
+    keyId: "arn:aws:kms:ap-northxxxxx",
+  });
 
-
-  //for keyId input the arn of the kms key that have to be used.
-  const kmsSigner= new AwsKmsSigner({region: 'ap-northeast-1', keyId: 'arn:aws:kms:ap-northxxxxx'});
-
-  const client = new FireflyClient(true, Networks.TESTNET_ARBITRUM, kmsSigner); //passing isTermAccepted = true for compliance and authorizarion
+  const client = new FireflyClient(true, Networks.TESTNET_ARBITRUM, kmsSigner); // passing isTermAccepted = true for compliance and authorizarion
   await client.init();
-  
+
   client.addMarket(MARKET_SYMBOLS.ETH);
-  
 
   const ethLeverage = await client.getUserDefaultLeverage(MARKET_SYMBOLS.ETH);
 
-  //KMS key provided will be used to sign this order and submit it to exchange
+  // KMS key provided will be used to sign this order and submit it to exchange
   const response = await client.postOrder({
     symbol: MARKET_SYMBOLS.ETH,
     price: 50,
     quantity: 0.5,
     side: ORDER_SIDE.BUY,
     leverage: ethLeverage,
-    orderType: ORDER_TYPE.LIMIT
-    });
+    orderType: ORDER_TYPE.LIMIT,
+  });
 
   console.log(response.data);
 }
---
+++ examples/Sockets/10-user-account-update.ts
@@ -0,0 +1,76 @@
+/**
+ * Places a market order on exchange and listens to emitted events
+ */
+
+/* eslint-disable no-console */
+import {
+  FireflyClient,
+  GetAccountDataResponse,
+  MARKET_SYMBOLS,
+  Networks,
+  ORDER_SIDE,
+  ORDER_TYPE,
+} from "../../index";
+
+async function main() {
+  // no gas fee is required to create order signature.
+  const dummyAccountKey =
+    "a182091b4d5a090b65d604e36f68629a692e3bf2aa864bd3f037854034cdd676";
+
+  const client = new FireflyClient(
+    true,
+    Networks.TESTNET_ARBITRUM,
+    dummyAccountKey
+  ); // passing isTermAccepted = true for compliance and authorizarion
+
+  // load/init contract addresses
+  await client.init();
+
+  client.addMarket(MARKET_SYMBOLS.ETH);
+
+  const callback = ({
+    accountData,
+  }: {
+    accountData: GetAccountDataResponse;
+  }) => {
+    console.log(accountData);
+
+    // kill sockets in order to stop script
+    client.sockets.close();
+  };
+  const connection_callback = async () => {
+    // This callback will be invoked as soon as the socket connection is established
+    // start listening to local user events
+    client.sockets.subscribeUserUpdateByToken();
+
+    // triggered when user account data updates are received
+    client.sockets.onUserAccountDataUpdate(callback);
+  };
+
+  const disconnection_callback = async () => {
+    console.log("Sockets disconnected, performing actions...");
+  };
+
+  // must specify connection_callback before opening the sockets below
+  await client.sockets.listen("connect", connection_callback);
+  await client.sockets.listen("disconnect", disconnection_callback);
+
+  console.log("Making socket connection to firefly exchange");
+  client.sockets.open();
+
+  /** ****  Placing an Order ***** */
+  // default leverage of account is set to 3 on firefly
+  const leverage = await client.getUserDefaultLeverage(MARKET_SYMBOLS.ETH);
+
+  // post a market order
+  await client.postOrder({
+    symbol: MARKET_SYMBOLS.ETH,
+    price: 0,
+    quantity: 0.5,
+    side: ORDER_SIDE.BUY,
+    orderType: ORDER_TYPE.MARKET,
+    leverage,
+  });
+}
+
+main().then().catch(console.warn);
