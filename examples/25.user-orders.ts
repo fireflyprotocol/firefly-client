dd1ec482ed309137c69234377158f02104b2ba2f
Danyal Khuaja <42298472+DanyalKhawaja@users.noreply.github.com>
2023-08-22 09:56:44 +0500

    added examples

---
+++ examples/25.user-orders.ts
@@ -0,0 +1,40 @@
+/**
+ * Get User Orders code example
+ */
+
+/* eslint-disable no-console */
+import { Networks, FireflyClient } from "../index";
+import { ORDER_STATUS } from "@firefly-exchange/library";
+
+async function main() {
+  // private key of the wallet
+  const dummyAccountKey =
+    "a182091b4d5a090b65d604e36f68629a692e3bf2aa864bd3f037854034cdd676";
+
+  const client = new FireflyClient(
+    true,
+    Networks.TESTNET_ARBITRUM,
+    dummyAccountKey
+  );
+
+  // load/init contract addresses
+  await client.init();
+
+  // fetch open orders of all markets
+  let resp = await client.getUserOrders({
+    statuses: [ORDER_STATUS.OPEN, ORDER_STATUS.PARTIAL_FILLED],
+  });
+  console.log(resp.data);
+
+  // fetch open orders of all markets
+  resp = await client.getUserOpenOrders({});
+  console.log(resp.data);
+
+  // fetch open limit orders of all markets
+  resp = await client.getUserOrdersByType({
+    limitStatuses: [ORDER_STATUS.OPEN, ORDER_STATUS.PARTIAL_FILLED],
+  });
+  console.log(resp.data);
+}
+
+main().then().catch(console.warn);
